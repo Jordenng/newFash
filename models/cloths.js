b9d2ed1d319d7e1767131a1d8aac6a4b7cfdd785
Yarden Nagel <nagel.yarden@gmail.com>
2021-08-31 16:00:47 +0300

    adding models

---
+++ models/cloths.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const ClothSchema = new Schema({
+    title: String,
+    price: String,
+    description: String,
+    location: String
+})
+
+module.exports=mongoose.model('Cloth', ClothSchema)

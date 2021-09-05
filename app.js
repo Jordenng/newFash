13bb577c68ee597bf19c54392990c7f15b11777b
Yarden Nagel <nagel.yarden@gmail.com>
2021-09-05 17:41:14 +0300

    intial commit

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
 const path = require('path')
 const Campground = require('./models/campgrounds')
 const morgan = require('morgan')
 
-app.use(morgan('tiny'))
 
 
-app.use((req,res,next)=>{
-    console.log(req.method.toUpperCase(), req.path)
-    next();
-});
+// app.use((req,res,next)=>{
+//     console.log(req.method.toUpperCase(), req.path)
+//     next();
+// });
 
-app.use('/dogs',(req,res,next)=>{
-    console.log("i love dogs")
-    next()
-})
+// app.use('/dogs',(req,res,next)=>{
+//     console.log("i love dogs")
+//     next()
+// })
 mongoose.connect('mongodb://localhost:27017/campgrounds',{
     useNewUrlParser: true,
     useUnifiedTopology:true
@@ -28,39 +26,60 @@ db.once("open",()=>{
     console.log("DB connected")
 })
 
+const app = express()
+
+app.use(morgan('tiny'))
+
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 
-// app.get('/',(req,res)=>{
-//     res.render('home')
+app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+
+app.get('/campgrounds',async(req,res)=>{
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds})
+})
+
+app.get('/campgrounds/new',(req,res)=>{
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds',async(req,res)=>{
+    const campground = new Campground(req.body.campground)
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+
+app.get('/campgrounds/:id',async(req,res)=>{
+    const camp = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', {camp})
+})
+
+
+// const verifyPassword = ((req,res,next)=>{
+//     const {password} = req.query;
+//     if (password === 'chicken'){
+//         next();
+//     }
+//     res.send("sorry you need a password")
 // })
-
-
-// app.get('/campgrounds',async(req,res)=>{
-//     const campground = new Campground({
-//         title: 'Shirt'})
-//         await campground.save()
-//     res.send(campground)
+//
+// app.get('/dogs',verifyPassword,(req,res)=>{
+//     res.send("woof")
 // })
-
-const verifyPassword = ((req,res,next)=>{
-    const {password} = req.query;
-    if (password === 'chicken'){
-        next();
-    }
-    res.send("sorry you need a password")
-})
-
-app.get('/dogs',verifyPassword,(req,res)=>{
-    res.send("woof")
-})
-
-app.get('/secret',verifyPassword,(req,res)=>{
-    res.send("my secret is.. you will never know.")
-})
-app.use((req,res)=>{
-    res.status(404).send("Not Found!")
-})
+//
+// app.get('/secret',verifyPassword,(req,res)=>{
+//     res.send("my secret is.. you will never know.")
+// })
+// app.use((req,res)=>{
+//     res.status(404).send("Not Found!")
+// })
 
 app.listen(3000,()=>{
     console.log("LISTENING ON PORT 8080")
